1ddf62f490f9d772faa560d9fc728ac23c2bad08
PraisePancakes <100659653+PraisePancakes@users.noreply.github.com>
2025-07-21 23:40:48 -0400

    redesigned compositor

--- include/ndo-incl/compositor.hpp
+++ include/ndo-incl/compositor.hpp
@@ -2,42 +2,21 @@
 #include "maybe.hpp"
 namespace ndo {
 
-template <typename T>
-class compositor {
-    ndo::maybe<T> value;
-
-   public:
-    compositor() : value{} {};
-    compositor(T&& t) : value{std::move(t)} {};
-    compositor(const T& t) : value(t) {};
-    compositor(const compositor& o) : value{std::move(o.value)} {};
-    compositor& operator=(const compositor& o) {
-        value = o.value;
-        return *this;
+struct compositor {
+    template <typename F>
+    [[nodiscard]] constexpr static decltype(auto) compose(F&& f) {
+        return std::forward<F>(f);
     };
-
-    compositor& operator=(const T& v) {
-        value = v;
-        return *this;
+    template <typename F, typename G, typename... Fs>
+    [[nodiscard]] constexpr static decltype(auto) compose(F&& f, G&& g, Fs&&... fs) {
+        return compose(std::forward<F>(f), compose(std::forward<G>(g), std::forward<Fs>(fs)...));
     };
-
     // x -> (f(g(x))) => y
-
     template <typename F, typename G>
-    [[nodiscard]] constexpr decltype(auto) compose(F&& f, G&& g) {
-        using first_t = std::invoke_result_t<G, T>;
-        ndo::maybe<first_t> first = ndo::maybe<first_t>(value.map_or_throw(std::forward<G>(g)));
-        return first.map_or_throw(std::forward<F>(f));
+    [[nodiscard]] constexpr static decltype(auto) compose(F&& f, G&& g) {
+        return [_f = std::forward<F>(f), _g = std::forward<G>(g)](auto&& x) -> decltype(auto) {
+            return _f(_g(std::forward<decltype(x)>(x)));
+        };
     };
-
-    template <typename F, typename G>
-    [[nodiscard]] constexpr decltype(auto) compose_safely(F&& f, G&& g) noexcept {
-        using first_t = std::invoke_result_t<G, T>;
-        ndo::maybe<first_t> first = ndo::maybe<first_t>(value.map_or_default(std::forward<G>(g)));
-        return first.map_or_default(std::forward<F>(f));
-    };
-
-    ~compositor() = default;
 };
-
 };  // namespace ndo
--- test/test-incl/test_compositor.hpp
+++ test/test-incl/test_compositor.hpp
@@ -5,8 +5,10 @@
 
 namespace ndo::test {
 void test_compositor() {
-    ndo::compositor<int> v{10};
-    auto value = v.compose([](std::string v) { return (v[0] + v[1]); }, [](int v) { return "hi"; });
-    std::cout << "composite value : " << value << std::endl;
+    auto value = ndo::compositor::compose([](std::string v) { return (v[0] + v[1]); }, [](int v) { return "hi"; });
+    auto v1 = value(3);
+
+    auto value2 = ndo::compositor::compose([](int x) { return x * 2; }, [](std::string new_s) { return 10; }, [](std::string og) { return "hello"; });
+    auto v2 = value2("hi");
 };
 }  // namespace ndo::test
