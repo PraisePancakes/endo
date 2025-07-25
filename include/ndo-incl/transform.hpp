8b4730bff9fad8ebc29bcfdda5b513c1aaf45a79
PraisePancakes <100659653+PraisePancakes@users.noreply.github.com>
2025-07-24 23:55:27 -0400

    map_from nodiscard

--- include/ndo-incl/transform.hpp
+++ include/ndo-incl/transform.hpp
@@ -4,7 +4,7 @@ namespace ndo {
 
 template <typename Container, typename F>
     requires(std::is_invocable_v<F, typename Container::value_type>)
-constexpr auto map_from(const Container& cont, F&& f) noexcept(noexcept(std::is_nothrow_invocable_v<F, typename Container::value_type>)) {
+[[nodiscard]] constexpr auto map_from(const Container& cont, F&& f) noexcept(noexcept(std::is_nothrow_invocable_v<F, typename Container::value_type>)) {
     Container ret{};
     for (auto it = cont.cbegin(); it != cont.cend(); ++it) {
         ret.insert(ret.end(), std::move(std::invoke(std::forward<F>(f), *it)));
